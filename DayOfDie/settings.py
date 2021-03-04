baff5ef4cc622df7fde0c34440cca128bd738471
Adam <adamkberard@gmail.com>
2021-03-03 21:03:23 -0800

    minor changes

--- DayOfDie/settings.py
+++ DayOfDie/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '+8q*_rmne*livuzd^x=#@rc@9wxa_522vjdfcoi5yk(z^lp#h_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'cleanr-life.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'dayofdie.herokuapp.com']
 
 # ENVIRONMENT VARIABLES
 #SECRET_KEY = os.environ.get('SECRET_KEY')
